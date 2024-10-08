aac848cfb0fb2a70829ad1d551a32e68bbf5a183
luihing2004 <106980759+luihing2004@users.noreply.github.com>
2024-10-08 18:33:45 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function showSection(sectionId) {
     selectedSection.style.display = 'block';
 }
 
-// 创建新项目并添加到表格
+// 创建新项目并添加到发布项目表格
 function createProject() {
     var projectName = document.getElementById('projectName').value;
     var projectMentor = document.getElementById('projectMentor').value;
@@ -48,56 +48,129 @@ function createProject() {
         document.getElementById('projectMentor').value = '';
         document.getElementById('researchDirection').value = '';
         document.getElementById('joinedMax').value = '';
+
+        // 调用更新搜索项目表格的函数
+        loadProjectsForSearch();
     } else {
         alert("Please fill in all fields!");
+    }
+}
+
+
+// 在搜索项目页面显示可加入的项目
+function loadProjectsForSearch() {
+    var projectsTable = document.getElementById('projectsTable').getElementsByTagName('tbody')[0];
+    var searchProjectsTable = document.getElementById('searchProjectsTable').getElementsByTagName('tbody')[0];
+
+    searchProjectsTable.innerHTML = '';  // 清空之前的数据
+
+    for (var i = 0; i < projectsTable.rows.length; i++) {
+        var projectName = projectsTable.rows[i].cells[0].innerText;
+        var projectMentor = projectsTable.rows[i].cells[1].innerText;
+        var researchDirection = projectsTable.rows[i].cells[2].innerText;
+        var joinedMax = projectsTable.rows[i].cells[3].innerText;
+
+        var newRow = searchProjectsTable.insertRow();
+
+        newRow.insertCell(0).innerText = projectName;
+        newRow.insertCell(1).innerText = projectMentor;
+        newRow.insertCell(2).innerText = researchDirection;
+        newRow.insertCell(3).innerText = joinedMax;
+
+        var actionCell = newRow.insertCell(4);
+
+        // 申请加入按钮
+        var joinButton = document.createElement("button");
+        joinButton.innerText = "Apply to Join";
+        joinButton.onclick = function() {
+            alert("Applied to join " + projectName);
+        };
+        actionCell.appendChild(joinButton);
     }
 }
 
 // 修改项目
 function editProject(row) {
+    // 获取当前行的值
     var projectName = row.cells[0].innerText;
     var projectMentor = row.cells[1].innerText;
     var researchDirection = row.cells[2].innerText;
     var joinedMax = row.cells[3].innerText;
 
-    document.getElementById('projectName').value = projectName;
-    document.getElementById('projectMentor').value = projectMentor;
-    document.getElementById('researchDirection').value = researchDirection;
-    document.getElementById('joinedMax').value = joinedMax;
+    // 将单元格内容变为可编辑的输入框
+    row.cells[0].innerHTML = `<input type='text' value='${projectName}'>`;
+    row.cells[1].innerHTML = `<input type='text' value='${projectMentor}'>`;
+    row.cells[2].innerHTML = `<input type='text' value='${researchDirection}'>`;
+    row.cells[3].innerHTML = `<input type='text' value='${joinedMax}'>`;
 
-    row.parentNode.removeChild(row);
+    // 将操作按钮变为“保存”
+    var actionCell = row.cells[4];
+    actionCell.innerHTML = ''; // 清空操作单元格内容
+
+    var saveButton = document.createElement("button");
+    saveButton.innerText = "Save";
+    saveButton.onclick = function() {
+        saveEditedProject(row);
+    };
+    actionCell.appendChild(saveButton);
+
+    var cancelButton = document.createElement("button");
+    cancelButton.innerText = "Cancel";
+    cancelButton.onclick = function() {
+        cancelEditProject(row, projectName, projectMentor, researchDirection, joinedMax);
+    };
+    actionCell.appendChild(cancelButton);
 }
 
-// 保存个人信息
-function savePersonalInfo() {
-    var name = document.getElementById('name').value;
-    var studentID = document.getElementById('studentID').value;
-    var major = document.getElementById('major').value;
+// 保存修改后的项目
+function saveEditedProject(row) {
+    // 获取输入框的值并更新到表格中
+    row.cells[0].innerText = row.cells[0].querySelector('input').value;
+    row.cells[1].innerText = row.cells[1].querySelector('input').value;
+    row.cells[2].innerText = row.cells[2].querySelector('input').value;
+    row.cells[3].innerText = row.cells[3].querySelector('input').value;
 
-    if (name && studentID && major) {
-        document.getElementById('displayName').innerText = name;
-        document.getElementById('displayStudentID').innerText = studentID;
-        document.getElementById('displayMajor').innerText = major;
+    // 恢复操作按钮
+    var actionCell = row.cells[4];
+    actionCell.innerHTML = ''; // 清空操作单元格内容
 
-        document.getElementById('personalInfoForm').style.display = 'none';
-        document.getElementById('personalInfoDisplay').style.display = 'block';
-    } else {
-        alert("Please fill in all fields!");
-    }
+    var editButton = document.createElement("button");
+    editButton.innerText = "Edit";
+    editButton.onclick = function() {
+        editProject(row);
+    };
+    actionCell.appendChild(editButton);
+
+    var deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.onclick = function() {
+        row.parentNode.removeChild(row); // 删除行
+    };
+    actionCell.appendChild(deleteButton);
 }
 
-// 编辑个人信息
-function editPersonalInfo() {
-    document.getElementById('personalInfoForm').style.display = 'block';
-    document.getElementById('personalInfoDisplay').style.display = 'none';
-}
+// 取消编辑项目，恢复原始值
+function cancelEditProject(row, projectName, projectMentor, researchDirection, joinedMax) {
+    row.cells[0].innerText = projectName;
+    row.cells[1].innerText = projectMentor;
+    row.cells[2].innerText = researchDirection;
+    row.cells[3].innerText = joinedMax;
 
-// 删除个人信息
-function deletePersonalInfo() {
-    document.getElementById('displayName').innerText = '';
-    document.getElementById('displayStudentID').innerText = '';
-    document.getElementById('displayMajor').innerText = '';
+    // 恢复操作按钮
+    var actionCell = row.cells[4];
+    actionCell.innerHTML = ''; // 清空操作单元格内容
 
-    document.getElementById('personalInfoForm').style.display = 'block';
-    document.getElementById('personalInfoDisplay').style.display = 'none';
+    var editButton = document.createElement("button");
+    editButton.innerText = "Edit";
+    editButton.onclick = function() {
+        editProject(row);
+    };
+    actionCell.appendChild(editButton);
+
+    var deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.onclick = function() {
+        row.parentNode.removeChild(row); // 删除行
+    };
+    actionCell.appendChild(deleteButton);
 }
